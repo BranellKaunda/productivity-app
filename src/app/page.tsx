3fbf17e5cf836d6197edfd3a40185dcf3ef6be46
BranellKaunda <branellk@gmail.com>
2025-09-15 17:17:14 +0200

    fetch player from API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,193 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import React from "react";
+import ReactDOM from "react-dom";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [inputValue, setInputValue] = useState("");
+  const [data, setData] = useState([]);
+  const [isSearched, setIsSearched] = useState(false);
+  const [playerPick, setPlayerPick] = useState<PlayerDetails[]>([]); //player selected after searching
+  const [isAdded, setIsAdded] = useState(false);
+  const [playerImage, setPlayerImage] = useState("/images/player.png");
+  const [fetchImg, setFetchImg] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  interface PlayerDetails {
+    name: string;
+    id: number;
+  }
+
+  //type is going to be an array of playerDetails type
+  let player: PlayerDetails[] = [];
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      url: "https://free-api-live-football-data.p.rapidapi.com/football-players-search",
+      params: { search: `${inputValue}` },
+      headers: {
+        "x-rapidapi-key": "4e111a6802mshc49aeb0b7139532p1c2a8bjsn3cd88b0a1a67",
+        "x-rapidapi-host": "free-api-live-football-data.p.rapidapi.com",
+      },
+    };
+
+    const debounceFetch = setTimeout(async () => {
+      try {
+        const response = await axios.request(options);
+        setData(response.data.response.suggestions);
+
+        if (inputValue == "") {
+          return;
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }, 2000);
+
+    return () => clearTimeout(debounceFetch);
+  }, [inputValue]);
+
+  useEffect(() => {
+    setIsSearched(true);
+  }, [data]);
+
+  player = [...data];
+
+  const fecthImages = async (param: PlayerDetails) => {
+    setFetchImg(true);
+    //using selected player id to fetch player images from API
+    const options = {
+      method: "GET",
+      url: "https://free-api-live-football-data.p.rapidapi.com/football-get-player-logo",
+      params: { playerid: `${param.id}` },
+      headers: {
+        "x-rapidapi-key": "4e111a6802mshc49aeb0b7139532p1c2a8bjsn3cd88b0a1a67",
+        "x-rapidapi-host": "free-api-live-football-data.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      //response saved in variable "image". updated player image state
+      const image = response.data.response.url;
+      setPlayerImage(image);
+
+      //ReactDOM.preload(image, { as: "image" });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const addPlayer = (value: PlayerDetails): void => {
+    //add player to playerPick state variable
+    setIsAdded(true);
+
+    if (value) {
+      setPlayerPick((prev) => prev.concat(value));
+      fecthImages(value);
+    }
+  };
+
+  return (
+    <>
+      <nav>
+        <button className="menu-logo">
+          <Image
+            src="/images/menu.png"
+            alt="menu logo"
+            width={32}
+            height={32}
+            priority
+            style={{ objectFit: "cover" }}
+          />
+        </button>
+
+        <div className="points">
+          <div className="points-logo">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/images/points.png"
+              alt="points image"
+              width={22}
+              height={22}
+              style={{ objectFit: "cover" }}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+
+          <p>1236</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      </nav>
+
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search player"
+          onChange={handleChange}
+        />
+
+        <div className="search-img">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/images/search.png"
+            alt="search icon"
+            width={20}
+            height={20}
+            priority
+            style={{ objectFit: "cover" }}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        </div>
+
+        {/*mapping over the array to render only 3 top search results by using the index*/}
+        <div className="search-results">
+          {isSearched &&
+            player.map(
+              (value, i) =>
+                i < 3 && (
+                  <div
+                    key={value.id}
+                    onClick={() => {
+                      addPlayer(value);
+                    }}
+                  >
+                    {value.name}
+                  </div>
+                )
+            )}
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="player-img">
           <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+            src={playerImage}
+            alt="menu logo"
+            width={200}
+            height={200}
+            priority
+            quality={100}
+            style={{ objectFit: "cover", borderRadius: "4%" }}
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        </div>
+
+        <div>
+          {isAdded &&
+            playerPick.map((player) => (
+              <p key={player.id}>
+                {player.name} id:{player.id}
+              </p>
+            ))}
+        </div>
+
+        <div className="timer">
+          <h1>00:00</h1>
+        </div>
+
+        <button>Start</button>
+      </div>
+    </>
   );
 }
