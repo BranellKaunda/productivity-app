2349df0d1d9bffb5626b9083de2c351f9fec61de
BranellKaunda <branellk@gmail.com>
2025-09-23 17:03:45 +0200

    football pitch css

--- src/app/Team/page.tsx
+++ src/app/Team/page.tsx
@@ -8,24 +8,21 @@ interface PlayerDetails {
 
 const Team = ({ squad }: { squad: PlayerDetails[] }) => {
   const [subIn, setSubIn] = useState(""); //used to select player from list to add to position
-  const [subout, setSubOut] = useState("");
-  //const [selectedPlayer, setSelectedPlayer] = useState<string | null>(null);
   const [subbedPlayers, setSubbedPlayers] = useState<string[]>([]);
   const [positions, setPositions] = useState([
-    "gk",
-    "lb",
-    "cb",
-    "cb",
-    "rb",
-    "lm",
-    "rcm",
-    "lcm",
-    "rm",
-    "lf",
-    "rf",
+    "GK",
+    "LB",
+    "LCB",
+    "RCB",
+    "RB",
+    "LCM",
+    "RCM",
+    "LW",
+    "RW",
+    "RF",
+    "LF",
   ]);
   const [isClicked, setIsClicked] = useState(false);
-  // const [isSubbedOn, setIsSubbedOn] = useState(false);
 
   useEffect(() => {
     console.log(squad);
@@ -35,18 +32,18 @@ const Team = ({ squad }: { squad: PlayerDetails[] }) => {
     setIsClicked(false);
     const replacedPlayer = positions[index]; // get the player being replaced
 
-    const replacePlayer = [
-      ...positions.slice(0, index),
-      playerIn,
-      ...positions.slice(index + 1),
-    ]; //replaces value at specific index with the player from squad list (subIn)
-
-    setPositions((prev) =>
-      positions.includes(playerIn) ? prev : replacePlayer
-    ); //avoid adding the same player twice to the line up
-
-    // Remove replaced player from subbedPlayers
     if (isClicked) {
+      const replacePlayer = [
+        ...positions.slice(0, index),
+        playerIn,
+        ...positions.slice(index + 1),
+      ]; //replaces value at specific index with the player from squad list (subIn)
+
+      setPositions((prev) =>
+        positions.includes(playerIn) ? prev : replacePlayer
+      ); //avoid adding the same player twice to the line up
+
+      // subs a player off when a new player is added to its position index
       setSubbedPlayers((prev) =>
         prev.includes(replacedPlayer)
           ? prev.filter((p) => p !== replacedPlayer)
@@ -56,48 +53,47 @@ const Team = ({ squad }: { squad: PlayerDetails[] }) => {
   };
 
   return (
-    <div className="team-container">
-      <div className="line-up">
-        {positions.map((position, i) => (
-          <div key={i} onClick={() => substitution(subIn, i)}>
-            {position}
-          </div>
-        ))}
+    <>
+      <div className="team-container">
+        <ul className="line-up">
+          {positions.map((position, i) => (
+            <li key={i} onClick={() => substitution(subIn, i)}>
+              {position}
+            </li>
+          ))}
+        </ul>
+
+        <div className="box-circle"></div>
+        <div className="center-circle"></div>
       </div>
 
       <div className="squad-list">
         <h1>Squad List</h1>
         <ul>
-          {squad.map(
-            (player, i) =>
-              i < 12 && (
-                <li
-                  key={player.id}
-                  onClick={() => {
-                    setIsClicked(true);
-                    setSubIn(player.name);
-                    setSubbedPlayers((prev) =>
-                      subbedPlayers.includes(player.name) //keeps track of players subbed in
-                        ? prev
-                        : [...prev, player.name]
-                    );
-                    console.log(subbedPlayers);
-                  }}
-                  style={{
-                    color: subbedPlayers.includes(player.name)
-                      ? "green"
-                      : "black",
-                    opacity: subbedPlayers.includes(player.name) ? 0 : 1,
-                    transition: "opacity 0.3s ease",
-                  }}
-                >
-                  {player.name}
-                </li>
-              )
-          )}
+          {squad.map((player) => (
+            <li
+              key={player.id}
+              onClick={() => {
+                setIsClicked(true);
+                setSubIn(player.name);
+                setSubbedPlayers((prev) =>
+                  subbedPlayers.includes(player.name) //keeps track of players subbed in
+                    ? prev
+                    : [...prev, player.name]
+                );
+              }}
+              style={{
+                color: subbedPlayers.includes(player.name) ? "green" : "black",
+                opacity: subbedPlayers.includes(player.name) ? 0 : 1,
+                transition: "opacity 0.3s ease",
+              }}
+            >
+              {player.name}
+            </li>
+          ))}
         </ul>
       </div>
-    </div>
+    </>
   );
 };
 
